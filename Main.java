d8c7ac341230d63401e4001751c88ca28acd7d23
Theboral <110025150+Theboral-Ayyanar@users.noreply.github.com>
2022-08-19 15:10:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,71 @@
+
+package finding.number;
+
+import java.util.Scanner;
+
+ class Main  
+ {
+
+	public static void main(String[] args) 
+	{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter the number");
+			int i=sc.nextInt();
+			if((i>=100)&&(i<200)&&(i>1000)&&(i<=10000))
+			{
+				System.out.println("Level 1");
+			
+			}
+		
+			else if((i<=200)&&(i<=20000))
+	        { 
+		    System.out.println("Level 2");
+	
+	        }
+            
+			else if((i<=300)&&(i<=30000))
+            {
+	        System.out.println("Level 3");
+            }
+	}
+	
+ }	
+	       
+
+		
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		
+			
+			
+			
+
+		
+		
+
+
+
+
